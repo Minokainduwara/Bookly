f583d5946b169ed6aa0b7613afbecd854cee9db7
Minokainduwara <induwaraminoka@gmail.com>
2026-08-24 03:42:26 +0530

    Feat: Add Tags

--- src/__init__.py
+++ src/__init__.py
@@ -7,7 +7,9 @@ from src.books.routes import book_router
 version = "v1"
 
 app = FastAPI(
+    title = "Bookly",
+    description="A REST API for a book review web service",
     version=version
 )
 
-app.include_router(book_router, prefix="/api/{version}/books")
+app.include_router(book_router, prefix="/api/{version}/books",tags=['books'])
